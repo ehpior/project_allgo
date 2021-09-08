b693a7c67c1d259a9cfd96c8eeaebf9e380865b9
ehpior <gusrl0101@naver.com>
2021-09-08 17:06:06 +0900

    config - spring cloud config server 추가

---
+++ config/src/test/java/com/jhk/allgo/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhk.allgo.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
